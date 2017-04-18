11c39fd7caf33febe139d93e24c52e5df0b445cb
Daniela <Litababy@abv.bg>
2017-04-18 22:38:27 +0300

    Admin panel and Categories list, edit and delete implemented

--- src/OnlineShop/Controller/CategoryController.php
+++ src/OnlineShop/Controller/CategoryController.php
@@ -12,6 +12,7 @@ use OnlineShop\Form\CategoryType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @Route("/categories")
@@ -48,6 +49,71 @@ class CategoryController extends Controller
         return $this->render('/category/create.html.twig', array('form' => $form->createView()));
     }
 
+    /**
+     * @Route("/", name="admin_categories")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function listCategories()
+    {
+        $categories = $this->getDoctrine()->getRepository(Category::class)->findAll();
 
+        return $this->render('admin/category/list.html.twig', ['categories' => $categories]);
+    }
+
+    /**
+     * @Route("/edit/{id}",name="admin_categories_edit")
+     * @param $id
+     * @param Request $request
+     * @return Response
+     */
+
+    public function editCategory($id,Request $request)
+    {
+        $category=$this->getDoctrine()->getRepository(Category::class)->find($id);
+        $form=$this->createForm(CategoryType::class,$category);
+        $form->handleRequest($request);
+        if($form->isSubmitted()&&$form->isValid())
+        {
+            $em=$this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_categories');
+
+        }
+
+        return $this->render('admin/category/edit.html.twig',
+            ['category' => $category, 'form' => $form->createView()]);
+
+    }
+
+    /**
+     * @Route("/delete/{id}",name="admin_categories_delete")
+     * @param $id
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     */
+    public function deleteCategory($id, Request $request)
+    {
+        $category=$this->getDoctrine()->getRepository(Category::class)->find($id);
+        $form=$this->createForm(CategoryType::class,$category);
+        $form->handleRequest($request);
+        if($form->isSubmitted()&&$form->isValid())
+        {
+            $em=$this->getDoctrine()->getManager();
+            foreach ($category->getProducts() as $product){
+                $em->remove($product);
+            }
+            $em->remove($category);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_categories');
+
+        }
+
+        return $this->render('admin/category/delete.html.twig',
+            ['category' => $category, 'form' => $form->createView()]);
+
+    }
 
 }
